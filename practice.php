b83371b7dbc8ed524e3d6297291880b606a3f2c9
林本和士 <rihanesu@gmail.com>
2020-02-06 03:14:19 +0000

    php

---
+++ practice.php
@@ -0,0 +1,38 @@
+<?php
+// 課題１
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+// 課題２
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+
+echo $array_month[7];
+
+// 課題３
+$hello = "Hello,";
+$name = "kazushi";
+$world = "'s World!";
+echo $hello.$name.$world;
+
+// 課題４
+$tech_boost = "tech";
+$tech_boost .=" boost";
+echo $tech_boost;
+
+// 課題５
+$calendar_2018 = [                  //数字が変数の始まりになっている
+    "January" => "1月",             // {} → []
+    "February" => "2月",            //10月に""が無い
+    "March" => "3月",               //要素の取り出しに""が無い
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "Jury" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+];
+echo $calendar_2018["December"];
